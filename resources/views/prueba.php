a9fffd6da62f57ded939218b41d008c7630f1452
Adanck <adantalavera94@gmail.com>
2019-05-21 14:09:38 -0600

    Prueba de interfaz

---
+++ resources/views/prueba.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Prisma</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</head>
+<body>
+<div class="container-fluid">
+  <h2>Administrador</h2>
+  <div class="navbar-header">
+  <!--a id="menu-toggle" class="navbar-toggle" href="#" onclick="menu-toggle">
+   <span class="icon-bar"></span>
+			    <span class="icon-bar"></span>
+<span class="icon-bar"></span>
+  </a-->
+  <p>Use the .flex-column class to create a vertical nav:</p>
+
+
+  <div id="sidebar-wrapper" class="sidebar-toggle sidebar">
+				<ul class="sidebar-nav">
+					<li>
+                        <a class="accordion-toggle collapsed toggle-switch border" style="margin-left:0px;" data-toggle="collapse" href="#submenu-3">
+
+                            <span class="">Citas</span>
+                            <b style="margin-left:72px;" class="caret"></b>
+                        </a>
+						<ul style="list-style:none; margin:0 0 0 0; padding:0 0 0 0;" id="submenu-3" class="panel-collapse collapse panel-switch" role="menu">
+							<li><a class="border" href="#"><span><b class="caret-right"></b> Reservar&nbsp</span></a></li>
+                            <li><a class="border" href="#"><span><b class="caret-right"></b> Actuales&nbsp</span></a></li>
+							<li><a class="border" href="#"><span><b class="caret-right"></b> Futuras&nbsp&nbsp&nbsp</span></a></li>
+							<li><a class="border" href="#"><span><b class="caret-right"></b> Histórico</span></a></li>
+                        </ul>
+                    </li>
+					<li>
+		      			<a class="border" href="#">Pacientes<span class="glyphicon glyphicon-search right-aling-glyphicon-paciente"></a>
+		    		</li>
+		    		<li>
+		      			<a class="border" href="#">Horario<span class="glyphicon glyphicon-time right-aling-glyphicon-h"></a>
+		    		</li>
+					<li>
+						<a class="border" href="#">Contraseña<span class="glyphicon glyphicon-lock right-aling-lock"></a>
+				  	</li>
+		    		<li class="hide-button-side">
+		      			<a class="border" href="#">Salir<span class="glyphicon glyphicon-log-out right-aling-glyphicon-s"></a>
+		    		</li>
+		    		<li>
+      					<div class="logo-ucr-3"><img src="https://medios.ucr.ac.cr/medios/imagenes/2016/ucr.svg" style="width:120px; height:120px;"></div>
+      				</li>
+		  		</ul>
+</div>
+
+</body>
+</html>
